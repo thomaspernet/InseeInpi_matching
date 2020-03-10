2ea5dd85b0944ecfd71efcfe7a42fd0085b755a5
clembl76 <clembl76@users.noreply.github.com>
2020-03-10 09:08:16 +0100

    Enrich ETS with timestamp and evt type
    - Enrich ETS with timestamp and evt type Notebook
    - GZ result file for ETS Enriched
    - Log result file for ETS Enriched
    - Update of AWS Connector for connecting from LOCAL with credential
    - In progress : Fill EVT with Initial EVT  Data

--- Notebooks_matching/Match_inpi_insee/aws_connectors.py
+++ Notebooks_matching/Match_inpi_insee/aws_connectors.py
@@ -4,7 +4,7 @@ from sagemaker import get_execution_role
 import dask.dataframe as dd
 
 class aws_instantiate:
-    def __init__(self, instance_aws, bucket = None):
+    def __init__(self, instance_aws, bucket = None, credential = None):
         """
         instance_aws: Instance name:
             format -> 'https://calfdata.s3.eu-west-3.amazonaws.com'
@@ -13,7 +13,36 @@ class aws_instantiate:
         self.instance_aws = instance_aws
         self.bucket = bucket
         self.s3 = boto3.resource('s3')
+        self.credential = credential
 
+    def client_boto_json(self):
+        """
+        Instanciates a S3 client with the credentials in json
+        """
+        with open(self.credential,'r') as f:
+            load_cred=json.load(f)
+            key=load_cred['aws_access_key_id']
+            secret_=load_cred['aws_secret_access_key']
+            
+        client_=boto3.client('s3',
+                             aws_access_key_id=key,
+                             aws_secret_access_key=secret_,
+                            )
+        
+        return client_
+
+    def client_boto_csv(self):
+        """
+        Instanciates a S3 client with the credentials in csv
+        """
+        load_cred = pd.read_csv(self.credential)
+        key = load_cred.iloc[0,1]
+        secret_ = load_cred.iloc[1,1]
+        client_ = boto3.client('s3',
+                             aws_access_key_id = key,
+                             aws_secret_access_key = secret_,)
+        return client_
+    
     def uploadFileBucket(self,pathfile):
         """
         Download file from S3 bucket
@@ -58,52 +87,7 @@ class aws_instantiate:
         pathtofile = "{}/{}".format(self.instance_aws,path_file)
 
         return pathtofile
-
-    def load_df_dd(self, path_file, dtypes = None, usecols = None):
-        """
-        Upload file to Sagemaker as a Pandas dataframe
-        using Dask for lazy computation
-
-        args:
-        pathfile: list path where the filename is located in S3,
-        including filename
-        ex: INSEE/Stock/ETS/StockEtablissement_utf8.csv
-        dtypes: Type name or dict of column -> type, optional
-        usecols: Return a subset of the columns.
-        If list-like, all elements must either be positional
-        (i.e. integer indices into the document columns)
-        or strings that correspond to column names provided either
-        by the user in names or inferred from the document header row(s).
-
-        returns:
-        Pandas Dataframe
-
-        """
-        ### file extension
-
-        filename, file_extension = os.path.splitext(path_file)
-
-
-        pathtofile = "{}/{}".format(self.instance_aws,path_file)
-
-        if file_extension ==  '.csv':
-            df_dask= dd.read_csv(pathtofile,
-                                 usecols = usecols,
-                                 dtype = dtypes,
-                                 blocksize=None,
-                                 low_memory=False
-                            )
-        else:
-            df_dask= dd.read_csv(pathtofile,
-                                 usecols = usecols,
-                                 dtype = dtypes,
-                                 blocksize=None,
-                                 compression='gzip',
-                                 low_memory=False
-                            )
-
-        return df_dask
-
+    
     def load_insee(self, option ='etb', for_matching = True):
         """
         Load INSEE data from S3 bucket
