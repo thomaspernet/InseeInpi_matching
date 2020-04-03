08b06e9b5978444d2e2164b5e6d14adcaafdc7b8
clembl76 <clembl76@users.noreply.github.com>
2020-04-03 10:06:27 +0200

    bugfix in root name

--- Notebooks_matching/Data_preprocessed/programme_matching/utils.py
+++ Notebooks_matching/Data_preprocessed/programme_matching/utils.py
@@ -4,60 +4,101 @@ import datetime
 from datetime import datetime
 
 def get_file_infos(filename):
+    
+    """
+    Computes information that are in the filename to classify its content.
+    cf INPI files documentation here : https://coda.io/d/CreditAgricole_dCtnoqIftTn/INSEE-INPI_suTDp
 
-        f_s = filename.split('_')
+    args:
+         - filename: Any filename
+         ex : '0101_S1_20170504_11_obs.csv'
 
-        nature_=f_s[-1].split('.')[0]
-        suborigin_ = None
+    returns:
+        A list of informations : (nature_,origin_,suborigin_,year_,timestamp_).
 
-        date_=f_s[2]
-        year_=date_[0:4]
-        month_=date_[4:6]
-        day_=date_[6:9]
+    """
 
-        if filename.endswith('EVT.csv'):
-            nature_='ETS'
-            suborigin_ = 'EVT'
+    f_s = filename.split('_')
 
-        time_= f_s[3]    
-        if f_s[1].startswith('S'):
-            origin_='Stock'
-            time_='0000'
-            if str(year_) == '2017':
-                suborigin_ = 'Initial'
-            else:
-                suborigin_ = 'Partiel'
-        else :
-            origin_='Flux'
+    nature_=f_s[-1].split('.')[0]
+    suborigin_ = None
+
+    date_=f_s[2]
+    year_=date_[0:4]
+    month_=date_[4:6]
+    day_=date_[6:9]
+    
+    if filename.endswith('EVT.csv'):
+        nature_='ETS'
+        suborigin_ = 'EVT'
+
+    time_= f_s[3]    
+    if f_s[1].startswith('S'):
+        origin_='Stock'
+        time_='0000'
+        if str(year_) == '2017':
+            suborigin_ = 'Initial'
+        else:
+            suborigin_ = 'Partiel'
+    else :
+        origin_='Flux'
 
 
-        if suborigin_ is None:
-                suborigin_ ='NEW'
+    if suborigin_ is None:
+            suborigin_ ='NEW'
 
-        datetime_str=date_ + '_' + time_
-        timestamp_=datetime.strptime(datetime_str,"%Y%m%d_%H%M%S")
+    datetime_str=date_ + '_' + time_
+    timestamp_=datetime.strptime(datetime_str,"%Y%m%d_%H%M%S")
 
-        if nature_ == 'annuels':
-            nature_ = 'comptes_annuels'
-        nature_=nature_.upper()
+    if nature_ == 'annuels':
+        nature_ = 'comptes_annuels'
+    nature_=nature_.upper()
 
-        return (nature_,origin_,suborigin_,year_,timestamp_)
+    return (nature_,origin_,suborigin_,year_,timestamp_)
 
     
 def calc_dest(filename):
-        (nature_,origin_,suborigin_,year_,timestamp_) = get_file_infos(filename)
-        if origin_ == 'Flux':
-            root_path='/INPI/TC_1/01_donnee_source/Flux'
-            dest_path = "{}/{}/{}/{}".format(root_path,year_,nature_,suborigin_)
-        else:
-            root_path='/INPI/TC_1/01_donnee_source/Stock/Stock'
-            dest_path = "{}_{}/{}/{}".format(root_path,suborigin_,year_,nature_)
+    """
+    Computes path where the file should be saved in S3 given its filename.
 
-        return dest_path
+    args:
+         - filename: Any filename
+         ex : '0101_1_20170512_112544_10_ets_supprime_EVT.csv'
 
-    
+    returns:
+        dest_path in S3 (no / at start)
+        ex : 'INPI/TC_1/01_donnee_source/Flux/2017/ETS/EVT'
+
+    """     
+
+    (nature_,origin_,suborigin_,year_,timestamp_) = get_file_infos(filename)
+    if origin_ == 'Flux':
+        root_path='INPI/TC_1/01_donnee_source/Flux'
+        dest_path = "{}/{}/{}/{}".format(root_path,year_,nature_,suborigin_)
+    else:
+        root_path='INPI/TC_1/01_donnee_source/Stock/Stock'
+        dest_path = "{}_{}/{}/{}".format(root_path,suborigin_,year_,nature_)
+
+    return dest_path
+
+
 def add_source_info(filename,source_path,dest_path = None):
+    """
+    Add file information inside csv columns to make it available in case of further files agregating operations.
 
+    args:
+         - filename: Any filename
+         ex : '0101_S1_20170504_11_obs.csv'
+         - source_path : location of the input file.
+         ex: 'data/dl'
+         - dest_path (optional): location of the result file. By default, same as source_path.
+         ex: 'data/prep'
+
+    returns:
+        dest_full_path : location and name of the result file.
+        ex: 'data/prep/0101_S1_20170504_11_obs.csv'
+
+    """
     source_full_path="{}/{}".format(source_path,filename)
     if dest_path:
         if not os.path.isdir(dest_path):
@@ -80,9 +121,4 @@ def add_source_info(filename,source_path,dest_path = None):
         print(e)
         return False
     
-    return dest_full_path    
-    
-def unzip(zipfilename,path):
-        full_path = os.path.join(path, zip_filename)
-        with zipfile.ZipFile(full_path, 'r') as zip_ref:
-            zip_ref.extractall(dest_path)
+    return dest_full_path
